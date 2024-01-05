d33f3197a1fd8676263389dddd65c7fddbe8364e
Moje <jsMonte31@gmail.com>
2024-01-05 12:36:09 -0300

    ajuste

--- src/redux/actions/categories/categories.js
+++ src/redux/actions/categories/categories.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-
 import { GET_CATEGORIES_FAIL, GET_CATEGORIES_SUCCESS } from "./types";
 
 export const get_categories = () => async (dispatch) => {
@@ -14,6 +13,7 @@ export const get_categories = () => async (dispatch) => {
       `${process.env.REACT_APP_API_URL}/api/category/list`,
       config
     );
+
     if (res.status == 200) {
       dispatch({
         type: GET_CATEGORIES_SUCCESS,
